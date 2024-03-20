d39e87e0b9f329fc3b7eda0546f0d717b453712d
allan-fagundes-eldorado <143724330+allan-fagundes-eldorado@users.noreply.github.com>
2024-03-20 11:00:23 -0300

    Init view structure

--- frontend/src/views/HtmlToPdf.tsx
+++ frontend/src/views/HtmlToPdf.tsx
@@ -1,6 +1,16 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+import Title from "../components/Typography/Title";
 
 export default function HtmlToPdf() {
   return(
-    <p>HtmlToPdf</p>
+    <Container centerContent>
+      <Flex direction={"column"} marginTop={12}>
+        <Title text={"Convert HTML to PDF Document"} />
+        <Text>Convert web pages to PDF documents with gracefully.</Text>
+      </Flex>
+      <Flex direction={"column"} marginTop={12}>
+        
+      </Flex>
+    </Container>
   )
 }
