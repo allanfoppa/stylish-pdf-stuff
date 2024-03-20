92b81b555f2e7a500ea702a21bfe54f5ec228b94
allan-fagundes-eldorado <143724330+allan-fagundes-eldorado@users.noreply.github.com>
2024-03-20 10:36:22 -0300

    No Match route view update

--- frontend/src/views/NoMatch.tsx
+++ frontend/src/views/NoMatch.tsx
@@ -1,15 +1,34 @@
 import { Link } from "react-router-dom";
 import { PATHS } from "../enums/paths.enum";
-import { Container, Text } from "@chakra-ui/react";
-import Title from "../components/Typography/Title";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 
 export default function NoMatch() {
   return (
-    <Container centerContent paddingTop={112}>
-      <Title text="Nothing to see here!" />
-      <Text fontSize='18px' color='tomato' paddingTop={6}>
-        <Link to={PATHS.HOME}>Go to the home page</Link>
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h2"
+        size="2xl"
+        bgGradient="linear(to-r, gray.400, gray.600)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2}>
+        Page Not Found
       </Text>
-    </Container>
+      <Text color={'gray.500'} mb={6}>
+        The page you&apos;re looking for does not seem to exist
+      </Text>
+
+      <Button
+        colorScheme="gray"
+        bgGradient="linear(to-r, gray.400, gray.500, gray.600)"
+        color="white"
+        variant="solid"
+      >
+        <Link to={PATHS.HOME}>Go to the home page</Link>
+      </Button>
+    </Box>
   );
 }
